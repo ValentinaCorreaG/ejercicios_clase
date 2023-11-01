91dd26568a66d0608f4ade6eaeae014846750915
ValentinaCorreaG <91170440+ValentinaCorreaG@users.noreply.github.com>
2023-11-01 12:15:05 -0500

    "clase pasada"

---
+++ taller0/assets/js/introduccion.js
@@ -0,0 +1,13 @@
+//QUE MONDA 
+/*scripts ECMaScripts isomófico*/ 
+
+$name
+_name
+name
+nombrePersona
+
+let numero=1;
+let nombre = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+let niVariable=true;
+let mi_arreglo=[1,2,3,4];
+
