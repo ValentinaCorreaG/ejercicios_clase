caff8640571c84d076a72abab839b7e564fe7be6
ValentinaCorreaG <91170440+ValentinaCorreaG@users.noreply.github.com>
2023-11-01 12:56:15 -0500

    "archivos js"

--- taller0/assets/js/introduccion.js
+++ taller0/assets/js/introduccion.js
@@ -1,13 +1,15 @@
 //QUE MONDA 
 /*scripts ECMaScripts isomófico*/ 
 
-$name
-_name
-name
-nombrePersona
 
 let numero=1;
 let nombre = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
 let niVariable=true;
 let mi_arreglo=[1,2,3,4];
 
+console.log(`el numero ${numero} es muy bonito \n`+`qqqqqqqqq`)
+
+let num1= parseInt(prompt("digite el primer número"))
+let num2= parseInt(prompt("digite el segundo número"))
+
+console.log(`la suma es ${num1 + num2}`)
